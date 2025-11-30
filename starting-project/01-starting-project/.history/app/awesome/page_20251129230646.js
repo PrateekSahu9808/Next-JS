09f602071fc786fb8e660ab19fd9b09e28db38a9
Prateek@9808 <mailprateeksahu@gmail.com>
2025-11-30 22:30:55 +0530

    first-app

---
+++ starting-project/01-starting-project/.history/app/awesome/page_20251129230646.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const As = () => {
+  return <div>page</div>;
+};
+
+export default page;
---
+++ starting-project/01-starting-project/app/awesome/page.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AwesomePage = () => {
+  return <div>page----</div>;
+};
+
+export default AwesomePage;
