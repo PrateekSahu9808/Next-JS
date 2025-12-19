41ea7c393aa179621c89861e8412ce965dd1769d
Prateek@9808 <mailprateeksahu@gmail.com>
2025-12-19 14:28:29 +0530

    routing and rendering

---
+++ Routing and page rendering/01-starting-project/app/news/[slug]/not-found.js
@@ -0,0 +1,8 @@
+export default function NewsNotFoundPage() {
+  return (
+    <div>
+      <h1>Not Found</h1>
+      <p>Unfortunatly not able to find the news </p>
+    </div>
+  );
+}
---
+++ Routing and page rendering/01-starting-project/app/not-found.js
@@ -0,0 +1,8 @@
+export default function NotFoundPage() {
+  return (
+    <div>
+      <h1>Not Found</h1>
+      <p>The Requested Resource could not be found</p>
+    </div>
+  );
+}
